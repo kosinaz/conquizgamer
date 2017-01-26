461797498c390277d4926062da92c52a3819fa5b
Zoli <zoltan@kosina.hu>
2017-01-26 22:44:24 +0100

    Add template-based quiz generation

--- conquizgamerController.js
+++ conquizgamerController.js
@@ -1,15 +1,27 @@
 /*global conquizgamer, answerEvaluaterService */
 conquizgamer.controller('conquizgamerController', [
   '$scope',
+  '$http',
+  '$q',
   'answerEvaluaterService',
   'quizGeneratorService',
-  function ($scope, answerEvaluaterService, quizGeneratorService) {
+  function ($scope, $http, $q, answerEvaluaterService, quizGeneratorService) {
     'use strict';
+    $scope.templatesResponse = $http.get('templates.json');
+    $scope.gamesResponse = $http.get('games.json');
+    $q.all([$scope.gamesResponse, $scope.templatesResponse])
+      .then(function (values) {
+        $scope.templates = values[1].data;
+        $scope.games = values[0].data;
+        $scope.quiz = quizGeneratorService.generate(
+          $scope.templates,
+          $scope.games
+        );
+      });
     $scope.points = 0;
-    $scope.quiz = quizGeneratorService.generate();
-    $scope.evaluate = function (answer) {
+    /*$scope.evaluate = function (answer) {
       $scope.points += answerEvaluaterService.evaluate($scope.quiz, answer);
       $scope.quiz = quizGeneratorService.generate();
-    };
+    };*/
   }
 ]);
--- quizGeneratorService.js
+++ quizGeneratorService.js
@@ -1,28 +1,36 @@
 /*global conquizgamer*/
 conquizgamer.factory('quizGeneratorService', [
   '$http',
-  function ($http) {
+  '$filter',
+  function ($http, $filter) {
     'use strict';
-    var games = {};
-    $http.get('games.json').then(function (response) {
-      games = response.data;
-    });
     return {
-      generate: function () {
-        return games[0] ? {
-          question: 'What is the genre of ' + games[0].name + '?',
-          answers: [
-            games[0].genre,
-            games[1].genre,
-            games[2].genre,
-            games[3].genre
-          ],
-          correct: games[0].genre
-        } : {
-          question: '',
-          answers: [
-            'Start'
-          ]
+      generate: function (templates, games) {
+        var template, game, question, subject, answers, i, j;
+        templates = $filter('filter')(templates, {"subcategory": "is"});
+        template = templates[Math.floor(Math.random() * templates.length)];
+        games = $filter('filter')(games, function (value, index, array) {
+          return value.hasOwnProperty(template.category);
+        });
+        game = games.splice(Math.floor(Math.random() * games.length), 1)[0];
+        subject = game[template.category];
+        if (Array.isArray(subject)) {
+          subject = subject[Math.floor(Math.random() * subject.length)];
+        }
+        answers = [];
+        while (games.length > 0) {
+          j = Math.floor(Math.random() * games.length);
+          answers.push(games.splice(j, 1)[0].name);
+        }
+        answers.splice(
+          Math.floor(Math.random() * answers.length),
+          0,
+          game.name
+        );
+        question = template.question.replace('%s', subject);
+        return {
+          question: question,
+          answers: answers
         };
       }
     };
