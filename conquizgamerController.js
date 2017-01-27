2056c932e7c59c7f966a9cf63d8d74a5849878bb
Zoli <zoltan@kosina.hu>
2017-01-27 20:42:46 +0100

    Add evaluation colors and timeout

--- conquizgamerController.js
+++ conquizgamerController.js
@@ -5,6 +5,7 @@ conquizgamer.controller('conquizgamerController', [
   '$interval',
   '$q',
   '$scope',
+  '$timeout',
   'quizGeneratorService',
   function (
     $filter,
@@ -12,6 +13,7 @@ conquizgamer.controller('conquizgamerController', [
     $interval,
     $q,
     $scope,
+    $timeout,
     quizGeneratorService
   ) {
     'use strict';
@@ -30,31 +32,40 @@ conquizgamer.controller('conquizgamerController', [
       });
     $scope.points = 5;
     $interval(function () {
-      if ($scope.points > 0) {
+      if ($scope.points > 0 && !$scope.evaluating) {
         $scope.quiz.answers[
           $scope.quiz.answers.length - $scope.points
-        ].disabled = "disabled";
+        ].disabled = 'disabled';
         $scope.points -= 1;
         if ($scope.points === 0) {
-          $scope.fails += 1;
+          $scope.evaluate({text: ''});
         }
       }
     }, 3000);
     $scope.evaluate = function (answer) {
+      if ($scope.evaluating) {
+        return;
+      }
+      $scope.evaluating = true;
+      $scope.quiz.answers[0].color = 'w3-green';
       if ($scope.quiz.answers[0].text === answer.text) {
         $scope.totalPoints += $scope.points;
       } else {
+        answer.color = 'w3-red';
         $scope.fails += 1;
       }
-      $scope.quiz = quizGeneratorService.generate(
-        $scope.templates,
-        $scope.games
-      );
-      $scope.points = 5;
-      if ($scope.fails > 2) {
-        $scope.totalPoints = 0;
-        $scope.fails = 0;
-      }
+      $timeout(function () {
+        $scope.quiz = quizGeneratorService.generate(
+          $scope.templates,
+          $scope.games
+        );
+        $scope.points = 5;
+        if ($scope.fails > 2) {
+          $scope.totalPoints = 0;
+          $scope.fails = 0;
+        }
+        $scope.evaluating = false;
+      }, 3000);
     };
   }
 ]);
